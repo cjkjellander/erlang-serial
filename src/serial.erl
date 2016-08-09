8feee11b98d61b8f4744beb54ecaebe11bf801b8
Carl-Johan Kjellander <cjk@eyad>
2016-08-09 21:36:23 +0200

    add debugging

--- src/serial.erl
+++ src/serial.erl
@@ -55,7 +55,7 @@ process_options(Pid,[Opt|Opts]) ->
 
 init(Pid) ->
     process_flag(trap_exit,true),
-    Port = open_port({spawn,priv_dir()++"/bin/serial -erlang"},
+    Port = open_port({spawn,priv_dir()++"/bin/serial -erlang -debug"},
                      [binary,{packet,2}]),
     loop(Pid,Port).
 
